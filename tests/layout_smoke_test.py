29aa2278e8fa7f07e1df9753bf9ed4c334600e5b
Anthony Francis <centaur@logicalrobotics.com>
2026-04-27 02:10:28 -0400

    Make src-layout smoke test rely on installed package

--- tests/layout_smoke_test.py
+++ tests/layout_smoke_test.py
@@ -1,12 +1,5 @@
 import importlib
-import pathlib
-import sys
 import unittest
-
-ROOT = pathlib.Path(__file__).resolve().parents[1]
-SRC = ROOT / "src"
-if str(SRC) not in sys.path:
-    sys.path.insert(0, str(SRC))
 
 
 class SrcLayoutSmokeTest(unittest.TestCase):
